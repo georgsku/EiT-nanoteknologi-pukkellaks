7fbf280f94df87b25fc9f3d5108f03250388080b
Georg Skuggedal <georg@favo.no>
2022-02-23 15:03:20 +0100

    PEW

--- src/views/Fishtank/Fishtank.tsx
+++ src/views/Fishtank/Fishtank.tsx
@@ -5,34 +5,50 @@ import flower from '../../svg/flowers.svg'
 import flower2 from '../../svg/flower6.svg'
 import flower3 from '../../svg/flower3.svg'
 
+const pew = require("../../sounds/Pew.mp3");
+
+
+function playSound(event: any) {
+  const audio = new Audio(pew);
+  audio.play();
+
+  event.target.remove()
+  
+}
+
+function blueFishClicked() {
+  const el = document.getElementById("blueFish")
+  window.alert("Vi dreper ikke vanlig fisk, bare pukkelaks")
+}
 
 function Goals() {
   return (
     <section className='fishtank luftig'>
-      <img className="fish one" src={fish} alt="" />
-      <img className="fish two" src={fish} alt="" />
-      <img className="fish three" src={fish} alt="" />
-      <img className="fish four" src={fish} alt="" />
+      <p id="blueFish"></p>
+      <img onClick={playSound} className="fish one" src={fish} alt="" />
+      <img onClick={playSound} className="fish two" src={fish} alt="" />
+      <img onClick={playSound} className="fish three" src={fish} alt="" />
+      <img onClick={playSound} className="fish four" src={fish} alt="" />
 
-      <img className="fish one late" src={fish} alt="" />
-      <img className="fish two late" src={fish} alt="" />
-      <img className="fish three late" src={fish} alt="" />
-      <img className="fish four late" src={fish} alt="" />
+      <img onClick={playSound} className="fish one late" src={fish} alt="" />
+      <img onClick={playSound} className="fish two late" src={fish} alt="" />
+      <img onClick={playSound} className="fish three late" src={fish} alt="" />
+      <img onClick={playSound} className="fish four late" src={fish} alt="" />
 
-      <img className="fish one late2" src={fish} alt="" />
-      <img className="fish two late2" src={fish} alt="" />
-      <img className="fish three late2" src={fish} alt="" />
-      <img className="fish four late2" src={fish} alt="" />
+      <img onClick={playSound} className="fish one late2" src={fish} alt="" />
+      <img onClick={playSound} className="fish two late2" src={fish} alt="" />
+      <img onClick={playSound} className="fish three late2" src={fish} alt="" />
+      <img onClick={playSound} className="fish four late2" src={fish} alt="" />
 
-      <img className="fish blue one" src={fishBlue} alt="" />
-      <img className="fish blue two" src={fishBlue} alt="" />
-      <img className="fish blue three" src={fishBlue} alt="" />
-      <img className="fish blue four" src={fishBlue} alt="" />
+      <img onClick={blueFishClicked} className="fish blue one" src={fishBlue} alt="" />
+      <img onClick={blueFishClicked} className="fish blue two" src={fishBlue} alt="" />
+      <img onClick={blueFishClicked} className="fish blue three" src={fishBlue} alt="" />
+      <img onClick={blueFishClicked} className="fish blue four" src={fishBlue} alt="" />
 
-      <img className="fish one late3" src={fish} alt="" />
-      <img className="fish two late3" src={fish} alt="" />
-      <img className="fish three late3" src={fish} alt="" />
-      <img className="fish four late3" src={fish} alt="" />
+      <img onClick={playSound} className="fish one late3" src={fish} alt="" />
+      <img onClick={playSound} className="fish two late3" src={fish} alt="" />
+      <img onClick={playSound} className="fish three late3" src={fish} alt="" />
+      <img onClick={playSound} className="fish four late3" src={fish} alt="" />
 
       <img className="left" src={flower3} alt="" />
       <img className="center" src={flower2} alt="" />
--- src/views/Footer/Footer.tsx
+++ src/views/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import './Footer.scss';
 function Goals() {
   return (
     <section className='footer luftig'>
-      <p>Sondre Georg Lars Anneli Amalie Zaim Thomas</p>
+      <p>Sondre Georg Lars Anneli Amalie Zaim⭐ Thomas</p>
     </section>
   );
 }
